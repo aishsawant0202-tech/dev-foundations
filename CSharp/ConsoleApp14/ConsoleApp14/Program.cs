0718c478ba5c85d4b2f2a4a8f4e342e38be5a405
Aishwarya Sawant <aishsawant0202@gmail.com>
2026-03-09 22:10:54 +0100

    grouping in LINQ

---
+++ CSharp/ConsoleApp14/ConsoleApp14/Program.cs
@@ -0,0 +1,37 @@
+﻿class Program
+{
+    static void Main()
+    {
+        List<Student> students = new List<Student>()
+        {
+            new Student{ Id = 1, Name = "Alice", Age = 20 },
+            new Student{ Id = 2, Name = "Bob", Age = 22 },
+            new Student{ Id = 3, Name = "Charlie", Age = 21 }
+        };
+
+        var result = students.Where(s => s.Age > 20).Select(s => new { s.Id, s.Name, s.Age });
+        foreach (var student in result)
+        {
+            Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Age: {student.Age}");
+        }
+
+        var studentAge = students.GroupBy(s => s.Age);
+        Console.WriteLine("\nStudents grouped by age:");
+        foreach (var group in studentAge)
+        {
+            Console.WriteLine($"Age: {group.Key}");
+            foreach (var student in group)
+            {
+                Console.WriteLine($"  Id: {student.Id}, Name: {student.Name}");
+            }
+
+        }
+    }
+    class Student
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+
+    }
+}
