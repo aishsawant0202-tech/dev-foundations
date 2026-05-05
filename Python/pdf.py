ce129a0c6e10bf8fb6dfb477441b0b9d38695350
Aishwarya Sawant <aishsawant0202@gmail.com>
2026-05-05 17:40:43 +0200

    python pdf methods

--- Python/pdf.py
+++ Python/pdf.py
@@ -1,18 +1,31 @@
 import PyPDF2
-import sys
 
-inputs = sys.argv[1:]
+template = PyPDF2.PdfReader(open('combined.pdf', 'rb'))
+watermark = PyPDF2.PdfReader(open('watermark.pdf', 'rb'))
+output = PyPDF2.PdfWriter()
+
+for i in range(len(template.pages)):
+    page = template.pages[i]
+    page.merge_page(watermark.pages[0])
+    output.add_page(page)
+
+with open('watermarked.pdf', 'wb') as file:
+    output.write(file)
+
+# import sys
+
+# inputs = sys.argv[1:]
 
 
-def pdf_combiner(pdf_list):
-    merger = PyPDF2.PdfMerger()
-    for pdf in pdf_list:
-        print(pdf)
-        merger.append(pdf)
-    merger.write('combined.pdf')
+# def pdf_combiner(pdf_list):
+#     merger = PyPDF2.PdfMerger()
+#     for pdf in pdf_list:
+#         print(pdf)
+#         merger.append(pdf)
+#     merger.write('combined.pdf')
 
 
-pdf_combiner(inputs)
+# pdf_combiner(inputs)
 
 
 # with open('DummyFile.pdf', 'rb') as file:
