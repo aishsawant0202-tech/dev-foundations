2f5be6494764fe94781edbc5c6e89154c7db172c
Aishwarya Sawant <aishsawant0202@gmail.com>
2026-05-13 06:27:10 +0200

    Data Scrapping in python

--- Python/scrape.py
+++ Python/scrape.py
@@ -1,9 +1,28 @@
 import requests
 from bs4 import BeautifulSoup
+from sympy import li
+import pprint
 
 response = requests.get('https://news.ycombinator.com/')
 soup = BeautifulSoup(response.text, 'html.parser')
-links = soup.select('.titleline')[1]
-print(links)
-votes = soup.select('.score')
-print(votes[1].get)
+links = soup.select('.titleline')
+subtext = soup.select('.subtext')
+
+
+def sort_stoeries_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k['votes'], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for idx, item in enumerate(links):
+        title = item.getText()
+        href = item.find('a').get('href', None)
+        vote = subtext[idx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            hn.append({'title': title, 'link': href, 'votes': points})
+    return sort_stoeries_by_votes(hn)
+
+
+pprint.pprint(create_custom_hn(links, subtext))
