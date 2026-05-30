afd0b2f84388254e8cfab85a8a9693d17281cf2f
Aishwarya Sawant <aishsawant0202@gmail.com>
2026-05-30 19:58:38 +0200

    website building using python

--- Python/webserver/server.py
+++ Python/webserver/server.py
@@ -2,10 +2,14 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 print(__name__)
 
-@app.route('/')
-def hello_world():
-    return render_template('index.html')
+@app.route('/<username>/<int:post_id>')
+def hello_world(username, post_id):
+    return render_template('index.html', username=username, post_id=post_id)
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+# @app.route('/icon.png')
+# def blog():
+#     return render_template('icon.png')
